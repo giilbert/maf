973f5e6d6ab6a09dfdcc25ae18a2d5493a803fee
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-03-17 22:26:43 -0400

    ee

--- crates/maf_container/src/runtime/wasi/mod.rs
+++ crates/maf_container/src/runtime/wasi/mod.rs
@@ -1,4 +1,5 @@
 use crate::container::ContainerData;
+use wasmtime::component::Resource;
 
 mod generated {
     wasmtime::component::bindgen!({
@@ -13,75 +14,55 @@ mod generated {
 
 pub use generated::maf::bindings::bindings::*;
 pub use generated::Imports as Bindings;
-use wasmtime_wasi::IoImpl;
 
 impl HostFutureConnection for ContainerData {
-    async fn drop(
-        &mut self,
-        rep: wasmtime::component::Resource<FutureConnection>,
-    ) -> wasmtime::Result<()> {
+    async fn drop(&mut self, connection: Resource<FutureConnection>) -> wasmtime::Result<()> {
         todo!();
     }
 
-    async fn get(
-        &mut self,
-        self_: wasmtime::component::Resource<FutureConnection>,
-    ) -> wasmtime::component::Resource<User> {
+    async fn get(&mut self, connection: Resource<FutureConnection>) -> Resource<User> {
         todo!();
     }
 
-    async fn subscribe(
-        &mut self,
-        self_: wasmtime::component::Resource<FutureConnection>,
-    ) -> wasmtime::component::Resource<Pollable> {
+    async fn subscribe(&mut self, connection: Resource<FutureConnection>) -> Resource<Pollable> {
         todo!();
     }
 }
 
 impl HostFutureRequest for ContainerData {
-    async fn drop(
-        &mut self,
-        rep: wasmtime::component::Resource<FutureRequest>,
-    ) -> wasmtime::Result<()> {
+    async fn drop(&mut self, request: Resource<FutureRequest>) -> wasmtime::Result<()> {
         todo!();
     }
 
-    async fn get(&mut self, self_: wasmtime::component::Resource<FutureRequest>) -> Request {
+    async fn get(&mut self, request: Resource<FutureRequest>) -> Request {
         todo!();
     }
 
-    async fn subscribe(
-        &mut self,
-        self_: wasmtime::component::Resource<FutureRequest>,
-    ) -> wasmtime::component::Resource<Pollable> {
+    async fn subscribe(&mut self, request: Resource<FutureRequest>) -> Resource<Pollable> {
         todo!();
     }
 }
 
 impl HostUser for ContainerData {
-    async fn drop(&mut self, rep: wasmtime::component::Resource<User>) -> wasmtime::Result<()> {
+    async fn drop(&mut self, user: Resource<User>) -> wasmtime::Result<()> {
         todo!();
     }
 
-    async fn new(&mut self, id: (u64, u64)) -> wasmtime::component::Resource<User> {
+    async fn new(&mut self, id: (u64, u64)) -> Resource<User> {
         todo!();
     }
 
-    async fn send(
-        &mut self,
-        self_: wasmtime::component::Resource<User>,
-        bytes: wasmtime::component::__internal::Vec<u8>,
-    ) -> Result<(), ()> {
+    async fn send(&mut self, user: Resource<User>, bytes: Vec<u8>) -> Result<(), ()> {
         todo!();
     }
 }
 
-impl generated::maf::bindings::bindings::Host for ContainerData {
-    async fn next_connection(&mut self) -> wasmtime::component::Resource<FutureConnection> {
+impl Host for ContainerData {
+    async fn next_connection(&mut self) -> Resource<FutureConnection> {
         todo!();
     }
 
-    async fn next_request(&mut self) -> wasmtime::component::Resource<FutureRequest> {
+    async fn next_request(&mut self) -> Resource<FutureRequest> {
         todo!();
     }
 }
