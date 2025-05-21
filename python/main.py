72ad346562045b05cd8ed40c889c48e7cb3431fe
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-05-21 09:34:55 -0400

    stores

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 import asyncio
 from websockets import connect
+from client import MafClient
 import logging
 
 logging.basicConfig(
@@ -7,27 +8,41 @@ logging.basicConfig(
     level=logging.INFO,
 )
 
-from packet import *
 
 async def main():
-    async with connect("ws://localhost:3000/@/hello/hello/connect") as ws:
-        await ws.send(TxHandshakePacket(
-            data = TxHandshakeData(
-                auth="hello"
-            ),
-        ).to_json())
+    async with MafClient(
+        url="http://localhost:3000",
+        app="gilbert/test-2",
+    ) as client:
 
-        response = RxPacket(await ws.recv())
-        print(f"Received from server: {response}")
+        async def counter_store():
+            store = client.store("example_basic::CounterStore")
+            async for data in store.changed():
+                print(f"store changed: {data}")
 
-        async for message in ws:
-            response = RxPacket(message)
+        asyncio.create_task(counter_store())
 
-            if response.type == "StoreUpdate":
-                data = response.store_update()
-                print(f"StoreUpdate data: {data}")
+        pass
 
-            print(f"Received from server: {response}")
+    # async with connect("ws://localhost:3000/@/hello/hello/connect") as ws:
+    #     await ws.send(TxHandshakePacket(
+    #         data = TxHandshakeData(
+    #             auth="hello"
+    #         ),
+    #     ).to_json())
+
+    #     response = RxPacket(await ws.recv())
+    #     print(f"Received from server: {response}")
+
+    #     async for message in ws:
+    #         response = RxPacket(message)
+
+    #         if response.type == "StoreUpdate":
+    #             data = response.store_update()
+    #             print(f"StoreUpdate data: {data}")
+
+    #         print(f"Received from server: {response}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- python/packet.py
+++ python/packet.py
@@ -5,24 +5,29 @@ from typing import TypeVar, Literal, Any
 
 T = TypeVar("T")
 
+
 @dataclass
 class RxHandshakeData:
     id: str
+
 
 @dataclass
 class RxChannelSendData:
     channel: str
     data: Any
 
+
 @dataclass
 class RxStoreUpdateData:
     store: str
     data: Any
 
+
 @dataclass
 class RxTypedRpcResponseData:
     id: str
     result: Any
+
 
 @dataclass
 class RxPacket:
@@ -33,11 +38,11 @@ class RxPacket:
         raw = json.loads(data)
         self.data = raw.get("data")
         self.type = raw.get("type")
-    
+
     def handshake(self) -> RxHandshakeData:
         assert self.type == "Handshake"
         return RxHandshakeData(**self.data)
-    
+
     def channel_send(self) -> RxChannelSendData:
         assert self.type == "ChannelSend"
         return RxChannelSendData(**self.data)
@@ -54,11 +59,13 @@ class RxPacket:
         assert self.type == "TypedRpcResponse"
         return RxTypedRpcResponseData(**self.data)
 
+
 class DataclassJSONEncoder(json.JSONEncoder):
     def default(self, o):
         if dataclasses.is_dataclass(o):
             return dataclasses.asdict(o)
         return super().default(o)
+
 
 @dataclass
 class TxPacket:
@@ -70,11 +77,15 @@ class TxPacket:
         self.type = type
 
     def to_json(self) -> str:
-        return json.dumps({"data": self.data, "type": self.type}, cls=DataclassJSONEncoder)
-    
+        return json.dumps(
+            {"data": self.data, "type": self.type}, cls=DataclassJSONEncoder
+        )
+
+
 @dataclass
 class TxHandshakeData:
     auth: T
+
 
 @dataclass
 class TxHandshakePacket(TxPacket):
