12be1e0046c73979d71376454f59ab2784dc5622
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-07-21 21:48:20 -0400

    make @usemaf/react package

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -1,19 +1,4 @@
 import { MafClient, type MafClientOptions } from "./client";
-import { MafProvider, type MafProviderProps } from "./maf-provider";
 import { Store, type StoreOptions, type StoreEvents } from "./store";
-import { useStore, type StoreStatus } from "./use-store";
-import { useRPC, type UseRPC } from "./use-rpc";
 
-export {
-  MafClient,
-  MafClientOptions,
-  MafProvider,
-  MafProviderProps,
-  Store,
-  StoreOptions,
-  StoreEvents,
-  useStore,
-  StoreStatus,
-  useRPC,
-  UseRPC,
-};
+export { MafClient, MafClientOptions, Store, StoreOptions, StoreEvents };
---
+++ packages/react/src/index.ts
@@ -0,0 +1,5 @@
+import { MafProvider, type MafProviderProps } from "./maf-provider";
+import { useStore, type StoreStatus } from "./use-store";
+import { useRPC, type UseRPC } from "./use-rpc";
+
+export { MafProvider, MafProviderProps, useStore, StoreStatus, useRPC, UseRPC };
