0641dd94f0a75dbcf63144896e8561cb6bca1523
SantiagoVira <santiagovira06@gmail.com>
2025-07-25 18:40:17 -0400

    improve provider

--- packages/react/src/maf-provider.tsx
+++ packages/react/src/maf-provider.tsx
@@ -1,9 +1,11 @@
 import { MafClient } from "@usemaf/client";
-import React, { createContext } from "react";
+import { ConnectOptions } from "@usemaf/client/src/client";
+import React, { createContext, useEffect, useRef, useState } from "react";
 
 export interface MafProviderProps {
   url: string;
   app: string;
+  connectOptions?: ConnectOptions;
 }
 
 export interface MafContextType {
@@ -14,13 +16,33 @@ export const MafContext = createContext<MafContextType | null>(null);
 
 export const MafProvider: React.FC<
   React.PropsWithChildren<MafProviderProps>
-> = ({ url, app, children }) => {
-  const client = new MafClient({ url, app });
-  client.connect();
+> = ({ url, app, connectOptions = { type: "default" }, children }) => {
+  const [mafClient] = useState<MafClient>(() => {
+    return new MafClient({ url, app });
+  });
 
-  const contextValue: MafContextType = { client };
+  useEffect(() => {
+    mafClient.connect(connectOptions);
+
+    return () => {
+      if (mafClient.ws.readyState === WebSocket.OPEN) {
+        mafClient.ws.close();
+      } else if (mafClient.ws.readyState === WebSocket.CONNECTING) {
+        mafClient.ws.onopen = () => {
+          mafClient.ws.close();
+        };
+      }
+    };
+  }, [url, app]);
 
   return (
-    <MafContext.Provider value={contextValue}>{children}</MafContext.Provider>
+    <MafContext.Provider
+      value={
+        {
+          client: mafClient,
+        } as MafContextType
+      }>
+      {children}
+    </MafContext.Provider>
   );
 };
