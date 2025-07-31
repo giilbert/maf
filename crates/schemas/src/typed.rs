842758c289c4c757e1281fd5667eb70b6662fffb
Gilbert Zhang <gilbertzhang1208@gmail.com>
2025-07-31 15:08:18 -0400

    convert facet types to serializable types

--- crates/schemas/src/typed.rs
+++ crates/schemas/src/typed.rs
@@ -1,25 +1,43 @@
+use facet::{StructKind, TextualType, UserType};
 use serde::Serialize;
 
-#[derive(Debug, Clone, Serialize)]
-pub enum Type {
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub struct Type {
+    pub name: String,
+    pub kind: TypeKind,
+}
+
+/// A high-level representation of types commonly used in programming languages.
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub enum TypeKind {
+    /// Numeric, boolean, character, string, etc.
     Primitive(PrimitiveType),
-    Nullable(Box<Type>),
+    /// Option<T> / `_ | null` types.
+    Nullable(Box<TypeKind>),
+
     Record(Box<RecordType>),
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
 pub enum PrimitiveType {
     Numeric(NumericType),
     Bool,
+    Char,
     String,
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
 pub struct RecordType {
-    pub fields: Vec<(String, Type)>,
+    pub fields: Vec<(String, TypeKind)>,
 }
 
-#[derive(Debug, Clone, Serialize)]
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
+pub struct MapType {
+    pub key: TypeKind,
+    pub value: TypeKind,
+}
+
+#[derive(Debug, Clone, Serialize, PartialEq, Eq)]
 pub enum NumericType {
     I8,
     I16,
@@ -33,4 +51,198 @@ pub enum NumericType {
     U128,
     F32,
     F64,
+}
+
+impl From<&'static facet::Shape> for TypeKind {
+    fn from(shape: &'static facet::Shape) -> Self {
+        match shape.ty {
+            facet::Type::Primitive(facet::PrimitiveType::Boolean) => {
+                TypeKind::Primitive(PrimitiveType::Bool)
+            }
+            facet::Type::Primitive(facet::PrimitiveType::Textual(TextualType::Char)) => {
+                TypeKind::Primitive(PrimitiveType::Char)
+            }
+            facet::Type::Primitive(facet::PrimitiveType::Textual(TextualType::Str)) => {
+                TypeKind::Primitive(PrimitiveType::String)
+            }
+            facet::Type::Primitive(facet::PrimitiveType::Numeric(numeric)) => {
+                let sized_layout = match shape.layout {
+                    facet::ShapeLayout::Sized(layout) => layout,
+                    _ => panic!("Numeric types must have a sized layout"),
+                };
+
+                match (numeric, sized_layout.size()) {
+                    (facet::NumericType::Integer { signed: true }, 1) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::I8))
+                    }
+                    (facet::NumericType::Integer { signed: true }, 2) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::I16))
+                    }
+                    (facet::NumericType::Integer { signed: true }, 4) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::I32))
+                    }
+                    (facet::NumericType::Integer { signed: true }, 8) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::I64))
+                    }
+                    (facet::NumericType::Integer { signed: true }, 16) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::I128))
+                    }
+                    (facet::NumericType::Integer { signed: false }, 1) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U8))
+                    }
+                    (facet::NumericType::Integer { signed: false }, 2) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U16))
+                    }
+                    (facet::NumericType::Integer { signed: false }, 4) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U32))
+                    }
+                    (facet::NumericType::Integer { signed: false }, 8) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U64))
+                    }
+                    (facet::NumericType::Integer { signed: false }, 16) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U128))
+                    }
+                    (facet::NumericType::Float, 4) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::F32))
+                    }
+                    (facet::NumericType::Float, 8) => {
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::F64))
+                    }
+                    _ => panic!("unsupported numeric type or size"),
+                }
+            }
+            // Special builtin types
+            facet::Type::User(UserType::Opaque) if shape.type_identifier == "String" => {
+                TypeKind::Primitive(PrimitiveType::String)
+            }
+            facet::Type::User(UserType::Opaque) if shape.type_identifier == "Option" => {
+                TypeKind::Nullable(Box::new((shape.type_params[0].shape)().into()))
+            }
+            facet::Type::User(UserType::Struct(struct_type))
+                if struct_type.kind == StructKind::Struct =>
+            {
+                TypeKind::Record(Box::new(RecordType {
+                    fields: struct_type
+                        .fields
+                        .iter()
+                        .map(|field| (field.name.to_string(), TypeKind::from(field.shape)))
+                        .collect(),
+                }))
+            }
+            facet::Type::Pointer(_) => unimplemented!("pointer types are not supported"),
+            other => todo!("unsupported type: {:?}", other),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use facet::Facet;
+
+    use super::*;
+
+    #[test]
+    fn serialize_primitive_types() {
+        assert_eq!(
+            TypeKind::from(u8::SHAPE),
+            TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U8))
+        );
+        assert_eq!(
+            TypeKind::from(i32::SHAPE),
+            TypeKind::Primitive(PrimitiveType::Numeric(NumericType::I32))
+        );
+        assert_eq!(
+            TypeKind::from(f64::SHAPE),
+            TypeKind::Primitive(PrimitiveType::Numeric(NumericType::F64))
+        );
+        assert_eq!(
+            TypeKind::from(bool::SHAPE),
+            TypeKind::Primitive(PrimitiveType::Bool)
+        );
+        assert_eq!(
+            TypeKind::from(char::SHAPE),
+            TypeKind::Primitive(PrimitiveType::Char)
+        );
+        assert_eq!(
+            TypeKind::from(str::SHAPE),
+            TypeKind::Primitive(PrimitiveType::String)
+        );
+        assert_eq!(
+            TypeKind::from(String::SHAPE),
+            TypeKind::Primitive(PrimitiveType::String)
+        );
+    }
+
+    #[test]
+    fn serialize_record_types() {
+        #[derive(Facet)]
+        struct Person {
+            pub name: String,
+            pub age: u32,
+        }
+
+        assert_eq!(
+            TypeKind::from(Person::SHAPE),
+            TypeKind::Record(Box::new(RecordType {
+                fields: vec![
+                    (
+                        "name".to_string(),
+                        TypeKind::Primitive(PrimitiveType::String)
+                    ),
+                    (
+                        "age".to_string(),
+                        TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U32))
+                    ),
+                ],
+            }))
+        );
+
+        #[derive(Facet)]
+        struct Student {
+            pub person: Person,
+            pub student_id: String,
+        }
+
+        assert_eq!(
+            TypeKind::from(Student::SHAPE),
+            TypeKind::Record(Box::new(RecordType {
+                fields: vec![
+                    (
+                        "person".to_string(),
+                        TypeKind::Record(Box::new(RecordType {
+                            fields: vec![
+                                (
+                                    "name".to_string(),
+                                    TypeKind::Primitive(PrimitiveType::String)
+                                ),
+                                (
+                                    "age".to_string(),
+                                    TypeKind::Primitive(PrimitiveType::Numeric(NumericType::U32))
+                                ),
+                            ],
+                        }))
+                    ),
+                    (
+                        "student_id".to_string(),
+                        TypeKind::Primitive(PrimitiveType::String)
+                    ),
+                ],
+            }))
+        );
+    }
+
+    #[test]
+    fn serialize_options() {
+        assert_eq!(
+            TypeKind::from(Option::<String>::SHAPE),
+            TypeKind::Nullable(Box::new(TypeKind::Primitive(PrimitiveType::String)))
+        );
+
+        assert_eq!(
+            TypeKind::from(Option::<i32>::SHAPE),
+            TypeKind::Nullable(Box::new(TypeKind::Primitive(PrimitiveType::Numeric(
+                NumericType::I32
+            ))))
+        );
+    }
 }
